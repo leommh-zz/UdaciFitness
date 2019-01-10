e7e03fc19231be18bcd13a32c308e080ad4d658c
Leonardo Morini <leo.morini.nn@gmail.com>
2019-01-10 20:48:06 -0200

    redux added
    feat: redux added
    
    In this release redux has been installed and configured to work on the following updates

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { View, Text, TouchableOpacity } from 'react-native'
-import { getMetricMetaInfo, timeToString } from '../utils/helpers'
+import { getMetricMetaInfo, timeToString, getDailyReminderValue } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers' 
 import DateHeader from './DateHeader'
 import TextButton from './TextButton'
 import { Ionicons } from '@expo/vector-icons'
 import { submitEntry, removeEntry } from '../utils/api'
+import { connect } from 'react-redux'
+import { addEntry } from '../actions'
 
 function SubmitBtn ({ onPress }) {
     return (
@@ -17,7 +19,7 @@ function SubmitBtn ({ onPress }) {
     )
 }
 
-export default class AddEntry extends Component {
+class AddEntry extends Component {
 
     state = {
         run: 0,
@@ -63,7 +65,9 @@ export default class AddEntry extends Component {
         const key = timeToString()
         const entry = this.state
 
-        //Update Redux
+        this.props.dispatch(addEntry({
+            [key]: entry
+        }))
 
         this.setState(() => ({
             run: 0,
@@ -83,7 +87,9 @@ export default class AddEntry extends Component {
     reset = () => {
         const key = timeToString()
 
-        //Update Redux
+        this.props.dispatch(addEntry({
+            [key]: getDailyReminderValue()
+        }))
 
         //Route to home
 
@@ -140,3 +146,12 @@ export default class AddEntry extends Component {
         )
     }
 }
+
+const mapStateToProps = (state) => {
+    const key = timeToString()
+    return {
+        alreadyLogged: state[key] && typeof state[key].today === 'undefined'
+    }
+}
+
+export default connect(mapStateToProps)(AddEntry)
