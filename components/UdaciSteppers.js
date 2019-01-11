054baaaa115864164e76775e46e285f34f594008
Leonardo Morini <leo.morini.nn@gmail.com>
2019-01-11 07:40:30 -0200

    new design
    style: remodeled screens

--- components/UdaciSteppers.js
+++ components/UdaciSteppers.js
@@ -1,22 +1,64 @@
 import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, Platform, StyleSheet } from 'react-native'
 import { FontAwesome, Entypo } from '@expo/vector-icons'
+import { white, red, orange, blue, lightPurp, pink } from './colors'
+import { purple } from '../utils/colors';
 
 export default function UdaciSteppers ({ max, unit, step, value, onIncrement, onDecrement }) {
     return (
-        <View>
-            <View>
-                <TouchableOpacity onPress={onDecrement}>
-                    <FontAwesome name='minus' size={30} color={'black'} />
+        <View style={[styles.row, { justifyContent: 'space-between' }]}>
+            <View style={{flexDirection: 'row'}}>
+                <TouchableOpacity
+                    style={
+                        Platform.OS === "ios" 
+                        ? [styles.iosBtn, { borderTopRightRadius: 0, borderBottomRightRadius: 0 }] 
+                        : styles.androidBtn
+                    }
+                    onPress={onDecrement}>
+                    <FontAwesome name='minus' size={30} color={purple} />
                 </TouchableOpacity>
-                <TouchableOpacity onPress={onIncrement}>
-                    <FontAwesome name='plus' size={30} color={'black'} />
+                <TouchableOpacity 
+                    style={
+                        Platform.OS === "ios" 
+                        ? [styles.iosBtn, { borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }] 
+                        : styles.androidBtn
+                    }
+                    onPress={onDecrement}>
+                    <FontAwesome name='plus' size={30} color={purple} />
                 </TouchableOpacity>
             </View>
-            <View>
-                <Text>{value}</Text>
-                <Text>{unit}</Text>
+            <View style={styles.metricCounter}>
+                <Text style={{fontSize: 24, textAlign: 'center'}}>{value}</Text>
+                <Text style={{fontSize: 18, color: gray}}>{unit}</Text>
             </View>    
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        flex: 1,
+        alignItems: 'center'
+    },
+    iosBtn: {
+        backgroundColor: white,
+        borderColor: purple,
+        borderWidth: 1,
+        borderRadius: 3,
+        padding: 5,
+        paddingLeft: 25,
+        paddingRight: 25,
+    },
+    androidBtn: {
+        margin: 5,
+        backgroundColor: purple,
+        padding: 10,
+        borderRadius: 2,
+    },
+    metricCounter: {
+        width: 85,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
